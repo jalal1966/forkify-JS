d0e14fe4aa338a317840eb586b8c4aa9e1ddf2e5
jalal bakir <jalalbak19666@gmail.com>
2025-02-19 19:16:48 +0100

    new KEY

--- src/js/config.js
+++ src/js/config.js
@@ -2,5 +2,5 @@ export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
 
 export const TIMOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = 'bef2cdb3-0546-44c4-bf39-ce20842b885c';
+export const KEY = 'fbbec632-20c0-4b79-9bd5-b5516f8ad12c';
 export const MODAL_CLOSE_SEC = 2.5;
